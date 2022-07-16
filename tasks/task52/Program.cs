5120bea72c9fc7ed50b5a453457aa47f7e1fec75
Anna Kerps <anya-kerps@bk.ru>
2022-07-16 14:53:58 +0300

    задача 52

---
+++ tasks/task52/Program.cs
@@ -0,0 +1,78 @@
+﻿/*  Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3   */
+
+int[,] GenerateArray(int rowNumber, int colNumber)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+void printArray(int[,] arrayToPrint)
+{
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j]);
+            if (j < arrayToPrint.GetLength(1) - 1)
+            {
+                Console.Write("\t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] arrayTrue = GenerateArray(row, column);
+printArray(arrayTrue);
+
+double[] average (int[,] array)
+{
+    double[] avgArray = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            avgArray[j] = sum / array.GetLength(0);
+        }
+    }
+    return avgArray;
+}
+
+void printAvgArray(double[] array)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1)
+        {
+            Console.Write($"{Math.Round(array[i],1)}; ");
+        }
+        else
+        {
+           Console.Write($"{Math.Round(array[i],1)}"); 
+        }
+        
+    }
+}
+
+double [] outputAverage = average(arrayTrue);
+printAvgArray(outputAverage);
